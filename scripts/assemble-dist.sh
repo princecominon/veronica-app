b77b869d8099e494b5d3ebdbdbf62a41f23b14a7
princecominon <prince.com.in.on@gmail.com>
2026-09-12 08:23:17 +0530

    script change

--- scripts/assemble-dist.sh
+++ scripts/assemble-dist.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")/.."
 DIST=dist
 rm -rf "$DIST"
 mkdir -p "$DIST"
-cp build/dist/js/productionExecutable/veronica.js "$DIST/"
+cp build/dist/js/productionExecutable/cargonex.js "$DIST/"
 cp web/index.html web/styles.css web/ship.svg "$DIST/"
 echo "Static site ready in ./$DIST"
